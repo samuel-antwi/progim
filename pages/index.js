8d40372fd8a0a390635076d765de3fc20bf7fa9c
Samuel Antwi <samuelantwidev@gmail.com>
2021-04-04 15:43:24 +0000

    set aos to animate once

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
 import Bmi from '../components/Bmi';
 import Discount from '../components/Discount';
 import FeaturedProducts from '../components/FeaturedProducts';
 import LandingPage from '../components/LandingPage';
 import Plan from '../components/Plan';
 import PopularClasses from '../components/PopularClasses';
-import SideNav from '../components/SideNav';
 import Trainers from '../components/Trainers';
 import WhatWeDo from '../components/WhatWeDo';
 import graphcms from '../graphql/client';
 import { PAGE_CONTENT, TRAINERS } from '../graphql/queries';
+import AOS from 'aos';
 
 export default function Home({ trainers, products, classes, pricePlans }) {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: function () {
+        const maxWidth = 800;
+        return window.innerWidth < maxWidth;
+      },
+    });
+  });
+
   return (
     <div>
       <LandingPage />
